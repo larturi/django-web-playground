3053259100037da76590279b53175dda735ddf69
Leandro Arturi <lea.arturi@gmail.com>
2022-01-25 20:30:59 -0300

    Logout

--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 
+# Para que haga el redirect al login custom y no al del admin
+# from django.contrib.auth.decorators import login_required
+# admin.autodiscover()
+# admin.site.login = login_required(admin.site.login)
+
 urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
