e36c9cc026d1be81b0b42c4b9c12ac14220786cc
Leandro Arturi <lea.arturi@gmail.com>
2022-01-24 22:38:20 -0300

    Decorator staff_member_required para validar CRUD solo para Staff

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -2,6 +2,9 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
 
@@ -14,12 +17,14 @@ class PageDetailView(DetailView):
     model = Page
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreateView(CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageUpdateView(UpdateView):
     model = Page
     form_class = PageForm
@@ -29,6 +34,7 @@ class PageUpdateView(UpdateView):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageDeleteView(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
