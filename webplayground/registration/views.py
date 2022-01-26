b39d89cde51359e0b5303b45eddab94757b0585b
Leandro Arturi <lea.arturi@gmail.com>
2022-01-26 19:11:40 -0300

    Campo email en Register

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationFormWithEmail
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     template_name = 'registration/signup.html'
 
     def get_success_url(self):
@@ -15,6 +15,7 @@ class SignUpView(CreateView):
         form = super(SignUpView, self).get_form()
 
         form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Nombre de usuario'})
+        form.fields['email'].widget = forms.EmailInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Email'})
         form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Password'})
         form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Repetir Password'})
 
