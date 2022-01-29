0eac5301f24b68fd06a5b0e0740432a19c0ef0d2
Leandro Arturi <lea.arturi@gmail.com>
2022-01-29 18:55:57 -0300

    Unit Test Profile

--- webplayground/registration/tests.py
+++ webplayground/registration/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='test', email='test@correo.com', password='test1234')
+
+    def test_profile_exists(self):
+        exists = Profile.objects.filter(user__username='test').exists()
+        self.assertEquals(exists, True)
