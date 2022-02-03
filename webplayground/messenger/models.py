d6331d57ccf06f2d616eb7a7f67ace55878cf7b7
Leandro Arturi <lea.arturi@gmail.com>
2022-02-03 13:22:49 -0300

    Fix para que un user que no pertenece a un hilo no pueda enviar mensajes

--- webplayground/messenger/models.py
+++ webplayground/messenger/models.py
@@ -1,6 +1,8 @@
 from email import message
+import imp
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
 
 
 class Message(models.Model):
@@ -15,3 +17,24 @@ class Message(models.Model):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+
+
+def messages_changed(sender, **kwargs):
+    instance = kwargs.pop('instance', None)
+    action = kwargs.pop('action', None)
+    pk_set = kwargs.pop('pk_set', None)
+
+    false_pk_set = set()
+
+    if action is "pre_add":
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if msg.user not in instance.users.all():
+                print("Ups, {} no forma parte del hilo".format(msg.user))
+                false_pk_set.add(msg_pk)
+
+    # Borro los mensajes invalidos
+    pk_set.difference_update(false_pk_set)
+
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
--- webplayground/messenger/tests.py
+++ webplayground/messenger/tests.py
@@ -7,6 +7,7 @@ class ThreadTestCase(TestCase):
     def setUp(self):
         self.user1 = User.objects.create_user('user1', None, 'test1234')
         self.user2 = User.objects.create_user('user2', None, 'test1234')
+        self.user3 = User.objects.create_user('user3', None, 'test1234')
 
         self.thread = Thread.objects.create()
 
@@ -28,6 +29,17 @@ class ThreadTestCase(TestCase):
         message1 = Message.objects.create(user=self.user1, content='hola')
         message2 = Message.objects.create(user=self.user2, content='chau')
         self.thread.messages.add(message1, message2)
+        self.assertEqual(len(self.thread.messages.all()), 2)
+
+        for message in self.thread.messages.all():
+            print("({}): {}".format(message.user, message.content))
+
+    def test_add_message_from_user_not_in_thread(self):
+        self.thread.users.add(self.user1, self.user2)
+        message1 = Message.objects.create(user=self.user1, content='hola')
+        message2 = Message.objects.create(user=self.user2, content='chau')
+        message3 = Message.objects.create(user=self.user3, content='soy un intruso')
+        self.thread.messages.add(message1, message2, message3)
         self.assertEqual(len(self.thread.messages.all()), 2)
 
         for message in self.thread.messages.all():
