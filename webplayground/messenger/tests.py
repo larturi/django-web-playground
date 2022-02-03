05227eeea48d16f574d93c972e0be15367fdb907
Leandro Arturi <lea.arturi@gmail.com>
2022-02-03 14:47:34 -0300

    Avances

--- webplayground/messenger/tests.py
+++ webplayground/messenger/tests.py
@@ -44,3 +44,15 @@ class ThreadTestCase(TestCase):
 
         for message in self.thread.messages.all():
             print("({}): {}".format(message.user, message.content))
+
+    def test_find_thread_with_custom_manager(self):
+        self.thread.users.add(self.user1, self.user2)
+        thread = Thread.objects.find(self.user1, self.user2)
+        self.assertEqual(self.thread, thread)
+
+    def test_find_or_create_thread_with_custom_manager(self):
+        self.thread.users.add(self.user1, self.user2)
+        thread = Thread.objects.find_or_create(self.user1, self.user2)
+        self.assertEqual(self.thread, thread)
+        thread = Thread.objects.find_or_create(self.user1, self.user3)
+        self.assertIsNotNone(thread)
